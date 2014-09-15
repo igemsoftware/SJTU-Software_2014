c494dcb8fa69d8dad427d0483c790a32161a87c6
DustInMonteriggioni <chyb1993@sina.cn>
2014-09-16 00:33:29 +0800

    Finish SearchRList filter, sort
    Oh, and also tried to solve the problem of SketchComponent class cast,
    found "return covariant", an interesting setting in Java after 1.5, but
    seems can't be used to solve the problem... 0.0 T.T 0.O O.0

--- src/data_center/SearchResultList.java
+++ src/data_center/SearchResultList.java
@@ -1,6 +1,8 @@
 package data_center;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 @SuppressWarnings("serial")
 public class SearchResultList extends ArrayList<BbkOutline>
@@ -28,7 +30,10 @@ public class SearchResultList extends ArrayList<BbkOutline>
 		return rows;
 	}
 
-	/** Use like "listToShow = rawList.filterByType().filterByEnterYear()...;" */
+	/** 
+	 * Use like "listToShow = rawList.filterByType().filterByEnterYear()...;" 
+	 * Please use the String in the SearchResultList.Filter
+	 */
 	public SearchResultList filterByType(ArrayList<String> typeList)
 	{	
 		if (typeList == null)	// don't filter
@@ -60,28 +65,204 @@ public class SearchResultList extends ArrayList<BbkOutline>
 		return filteredList;
 	}
 	
-	public SearchResultList filterBySthInRating1()
+	/** Please use the String in the SearchResultList.Filter */
+	public SearchResultList filterByRelaseStatus(String status)
 	{
-		// fix me
-		return null;
+		if ( !status.equals(Filter.ReleaseStatus.RELEASED) &&
+			 !status.equals(Filter.ReleaseStatus.NOT_RELEASED) &&
+			 !status.equals(Filter.ReleaseStatus.DELETED))
+		{	System.out.println("Invalid release status input... 0.0");
+			return (SearchResultList) this.clone();
+		}
+		// else
+		SearchResultList filteredList = new SearchResultList();
+		for (BbkOutline bbk : this)
+			if (bbk.releaseStatus.equals(status))
+				filteredList.add(bbk);
+		return filteredList;
 	}
 	
-	public SearchResultList filterBySthInRating2()
+	/** Please use the String in the SearchResultList.Filter */
+	public SearchResultList filterByDNAStatus(String status)
 	{
-		// fix me
-		return null;
+		if ( !status.equals(Filter.DNAStatus.AVAILABLE) &&
+			 !status.equals(Filter.DNAStatus.DELETED) &&
+			 !status.equals(Filter.DNAStatus.INFORMATIONAL) &&
+			 !status.equals(Filter.DNAStatus.PLANNING) &&
+			 !status.equals(Filter.DNAStatus.UNAVAILABLE))
+		{	System.out.println("Invalid DNA status input... 0.0");
+			return (SearchResultList) this.clone();
+		}
+		// else
+		SearchResultList filteredList = new SearchResultList();
+		for (BbkOutline bbk : this)
+			if (bbk.DNA_status.equals(status))
+				filteredList.add(bbk);
+		return filteredList;
 	}
 	
-	public SearchResultList filterBySthInRating3()
+	public SearchResultList filterByDeletedOrNot(boolean deleted)
 	{
-		// fix me
-		return null;
+		SearchResultList filteredList = new SearchResultList();
+		for (BbkOutline bbk : this)
+			if ( (bbk.rating.delete_this_part.equals(Filter.DeletedOrNot.DELETED)) == deleted)
+				filteredList.add(bbk);
+		return filteredList;
 	}
 	
-	public SearchResultList filterBySthInRating4()
+	/** 4 in starNumList means star number is [4, 5) */
+	public SearchResultList filterByStarNum(ArrayList<Integer> starNumList)
 	{
-		// fix me
-		return null;
+		SearchResultList filteredList = new SearchResultList();
+		for (BbkOutline bbk : this)
+			for (Integer starNum : starNumList)
+				if ( bbk.rating.average_stars.startsWith(starNum.toString()) )
+					filteredList.add(bbk);
+		return filteredList;
+	}
+	
+	/** Change the list in situ, DESC means ½µÐò  */
+	public void sortByEnterDate(final boolean DESC)
+	{	
+		Comparator<BbkOutline> comparator = new Comparator<BbkOutline>()
+		{
+			@Override
+			public int compare(BbkOutline bbk1, BbkOutline bbk2)
+			{
+				String[] YMD1 = bbk1.enterDate.split("/");	// year, month, date
+				String[] YMD2 = bbk2.enterDate.split("/");
+				int year1 = Integer.parseInt(YMD1[0]), year2 = Integer.parseInt(YMD2[0]);
+				int month1 = Integer.parseInt(YMD1[1]), month2 = Integer.parseInt(YMD2[1]);
+				int date1 = Integer.parseInt(YMD1[2]), date2 = Integer.parseInt(YMD2[2]);
+				if (DESC)
+				{	if (year1 != year2)
+						return year2 - year1;
+					else if (month1 != month2)
+						return month2 - month1;
+					else
+						return date2 - date1;
+				}
+				else
+				{	if (year1 != year2)
+						return year1 - year2;
+					else if (month1 != month2)
+						return month1 - month2;
+					else
+						return date1 - date2;
+				}
+			}
+		};
+		Collections.sort(this, comparator);
+	}
+	
+	/** Change the list in situ, DESC means ½µÐò  */
+	public void sortByGoogleQuoteNum(final boolean DESC)
+	{	
+		Comparator<BbkOutline> comparator = new Comparator<BbkOutline>()
+		{
+			@Override
+			public int compare(BbkOutline bbk1, BbkOutline bbk2)
+			{
+				int quoteNum1, quoteNum2;
+				try
+				{	quoteNum1 = Integer.parseInt(bbk1.rating.google_items);	}
+				catch (Exception e) {	quoteNum1 = 0;	}
+				try
+				{	quoteNum2 = Integer.parseInt(bbk2.rating.google_items);	}
+				catch (Exception e) {	quoteNum2 = 0;	}
+				if (DESC)
+					return quoteNum2 - quoteNum1;
+				else
+					return quoteNum1 - quoteNum2;
+			}
+		};
+		Collections.sort(this, comparator);
+	}
+	
+	/** Change the list in situ, DESC means ½µÐò  */
+	public void sortByAverageStars(final boolean DESC)
+	{	
+		Comparator<BbkOutline> comparator = new Comparator<BbkOutline>()
+		{
+			@Override
+			public int compare(BbkOutline bbk1, BbkOutline bbk2)
+			{
+				double star1, star2;
+				try
+				{	star1 = Double.parseDouble(bbk1.rating.average_stars);	}
+				catch (Exception e) {	star1 = 0;	}
+				try
+				{	star2 = Double.parseDouble(bbk2.rating.average_stars);	}
+				catch (Exception e) {	star2 = 0;	}
+				if (DESC)
+				{	if (star1 > star2)	return -1;
+					else if (star1 < star2) return 1;
+					else return 0;
+				}
+				else
+				{	if (star1 > star2)	return 1;
+					else if (star1 < star2) return -1;
+					else return 0;
+				}
+			}
+		};
+		Collections.sort(this, comparator);
+	}
+	
+	/** Change the list in situ, DESC means ½µÐò  */
+	public void sortByConfrimedTimes(final boolean DESC)
+	{	
+		Comparator<BbkOutline> comparator = new Comparator<BbkOutline>()
+		{
+			@Override
+			public int compare(BbkOutline bbk1, BbkOutline bbk2)
+			{
+				int confrimedTimes1, confrimedTimes2;
+				try
+				{	confrimedTimes1 = Integer.parseInt(bbk1.rating.tot_confirmed);	}
+				catch (Exception e) {	confrimedTimes1 = 0;	}
+				try
+				{	confrimedTimes2 = Integer.parseInt(bbk2.rating.tot_confirmed);	}
+				catch (Exception e) {	confrimedTimes2 = 0;	}
+				if (DESC)
+					return confrimedTimes2 - confrimedTimes1;
+				else
+					return confrimedTimes1 - confrimedTimes2;
+			}
+		};
+		Collections.sort(this, comparator);
+	}
+	
+	/** Change the list in situ, DESC means ½µÐò  */
+	public void sortByBlastResult(final boolean DESC)
+	{	
+		Comparator<BbkOutline> comparator = new Comparator<BbkOutline>()
+		{
+			@Override
+			public int compare(BbkOutline bbk1, BbkOutline bbk2)
+			{
+				if (bbk1.blasting == null || bbk2.blasting == null) // not blasted
+					return 0;
+				// else
+				if (DESC)
+				{	if (bbk1.blasting.eValue < bbk2.blasting.eValue) // 1 should close to the top of the list
+						return 1;
+					else if (bbk1.blasting.eValue > bbk2.blasting .eValue)
+						return -1;
+					else
+						return bbk2.blasting.score - bbk1.blasting.score;
+				}
+				else
+				{	if (bbk1.blasting.eValue < bbk2.blasting.eValue)
+						return -1;
+					else if (bbk1.blasting.eValue > bbk2.blasting .eValue)
+						return 1;
+					else
+						return bbk1.blasting.score - bbk2.blasting.score;
+				}
+			}
+		};
+		Collections.sort(this, comparator);
 	}
 	
 	public void display()
@@ -89,5 +270,53 @@ public class SearchResultList extends ArrayList<BbkOutline>
 		for (BbkOutline bbk : this)
 			bbk.display();
 	}
+	
+	
+	
+	public static class Filter
+	{	
+		public static class Type
+		{	
+			public final static String PROMOTER = "Regulatory";
+			public final static String RBS = "RBS";
+			public final static String PROTEIN_DOMAIN = "Tag";
+			public final static String PROTEIN_CODING_SEQUENCE = "Coding";
+			public final static String TRANSLATIONAL_UNIT = "Translational_Unit";
+			public final static String TERMINATOR = "Terminator";
+			public final static String DNA = "DNA";
+			public final static String PLASMID_BACKBONE = "Plasmid_Backbone";
+			public final static String PLASMID = "Plasmid";
+			public final static String PRIMER = "Primer";
+			public final static String COMPOSITE = "Composite";
+			public final static String PROTEIN_GENERATOR = "Generator";
+			public final static String REPORTER = "Reporter";
+			public final static String INVERTER = "Inverter";
+			public final static String SIGNALLING = "Signalling";
+			public final static String MEASUREMENT = "Measurement";
+		}
+		
+		public static class ReleaseStatus
+		{	
+			public final static String RELEASED = "Released HQ 2013";
+			public final static String NOT_RELEASED = "Not Released";
+			public final static String DELETED = "Deleted";
+		}
+		
+		public static class DNAStatus
+		{	
+			public final static String AVAILABLE = "Available";
+			public final static String DELETED = "Deleted";
+			public final static String INFORMATIONAL = "Informational";
+			public final static String PLANNING = "Planning";
+			public final static String UNAVAILABLE = "Unavailable";
+		}
+		
+		public static class DeletedOrNot
+		{	
+			public final static String DELETED = "Deleted";
+			public final static String NOT_DELETED = "Not Deleted";
+		}
+	}
+	
 	
 }
--- src/data_center/Testing.java
+++ src/data_center/Testing.java
@@ -1,73 +1,50 @@
 package data_center;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javax.swing.*;
 
 import data_center.SketchComponent.*;
 
+@SuppressWarnings("serial")
 public class Testing  extends JFrame
 {	
-	/*
-	JTable tableList =null;
-	String[] cols={"name","type","author","enterDate","shortDesc","url"};
-    String[][] rows=null;
-    public Testing()
-    {
-    	BbkDatabaseConnector conncetor = new BbkDatabaseConnector();
-    	//SearchResultList resultList = conncetor.search("B0034");
-    	SearchResultList resultList = conncetor.search("jcbraff","part_author");
-    	rows = resultList.showSearchResult();
-    	JPanel jp=(JPanel)this.getContentPane();
-    	tableList = new JTable(rows,cols);
-		JScrollPane jsp_table=new JScrollPane(tableList);
-		jp.add(jsp_table);
-		this.setTitle("Biobrick Search");
-		this.setSize(600,400);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    */
 	public static void main(String[] args)
 	{	
-		//Test for Search
-		//new Testing();
+		ArrayList<Component> componentList = new ArrayList<Component>();
+		componentList.add(new Label(0, null, null, null, null));
+		componentList.add(new BioBrick(0, 0, null, null));
 		
-		//SearchResultList list
-			//= BbkBlaster.blast_local(BbkBlaster.DEFAULT_INFILE, BbkBlaster.MODE_INPUT_FILE_PATH);
-		//for (int i = 0; i < 100; ++i)
-			//list.get(i).display();
+		Component component = new Label(0, null, null, null, null);
+		System.out.println(component.getClass() == Label.class);
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);list.add(2);list.add(3);
+		sort(list, false);
+		
+		try
+		{	Integer.parseInt("NULL");
+		} catch (Exception e){System.out.println("ei?");}
 		
 		
-		BbkDatabaseConnector connector = new BbkDatabaseConnector();
-		
-		//Test for ...
-		
-//		ArrayList<Component> componentList = new ArrayList<Component>();
-//		componentList.add(new Label(0, "Lable text", 
-//				new Point(5, 5), new Font("Times Roman", 10, 3), new Color(0, 0, 0)));
-//		componentList.add(new BioBrick(1, BbkType.Sketch.BioBrick.PROMOTER, 
-//				new Point(10, 10), null));
-//		componentList.add(new Protein(2, BbkType.Sketch.Protein.FACTOR, 
-//				new Point(20, 20), Color.BLUE));
-//		componentList.add(new BackBone(3, new Point(50, 50), new Point(100, 50)));
-//		componentList.add(new Relation(4, BbkType.Sketch.Relation.SUPPRESS, 
-//				new ArrayList<Point>(), new Color(50, 50, 50), 10));
-//		componentList.add(new Vector(5, BbkType.Sketch.Vector.BACTERIA, 
-//				new Point(300, 300), 3));
-		// 假设现在有这么一个componentList，需要将整个列表写入XML文件并后续再次读取。
-		// 注意里面的Color有两种表示方式Color(x, x, x)或者Color.BLACK，但记录在文件中应该
-		// 		是一样的形式
-		// 另外不同component类型（如列表中第1,2个加入的Label与BioBrick）也需要记录。这在
-		// 		数据结构中记录在父类component的primaryType中
-		// 不妨仔细看一下SketchComponent里面的结构
-		// 玮哥哥请完全无视本文件中的所有内容~
+	}
+	
+	public static void sort(ArrayList<Integer> list, final boolean DESC)
+	{	
+		Comparator<Integer> comparator = new Comparator<Integer>()
+		{
+			@Override
+			public int compare(Integer x1, Integer x2)
+			{
+				if (DESC)
+					return x2 - x1;
+				
+				else
+					return x1 - x2;
+			}
+		};
+		Collections.sort(list, comparator);
 	}
 }
